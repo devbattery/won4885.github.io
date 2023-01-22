815bfa6e1a9b4bdf207645a79d66902bafb7b40d
Sully <62871026+won4885@users.noreply.github.com>
2023-01-22 03:57:58 +0000

    Update navigation

--- replit.nix
+++ replit.nix
@@ -1,5 +1,8 @@
 { pkgs }: {
   deps = [
+    pkgs.sudo
+    pkgs.print(str(datetime.datetime.now(timezone)))\
+    pkgs.sudo
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server
   ];
